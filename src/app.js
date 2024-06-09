391b128e0bbdec8b058c1d0efa69f6b6bdc9b90f
matlvy <145528395+matlvy@users.noreply.github.com>
2024-06-09 03:13:48 -0300

    8.3

--- src/app.js
+++ src/app.js
@@ -19,3 +19,20 @@ app.use("/api/pets", petsRouter);
 app.use("/api/users", usersRouter);
 
 app.use(express.static("./src/public"));
+
+import multer from "multer";
+
+const storage = multer.diskStorage({
+  destination: (req2, file2, cb2) => {
+    cb(null, "src/public/img");
+  },
+  filename: (req3, file3, cb3) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage });
+
+app.post("/upload", upload.single("image"), (req, res) => {
+  res.send("uploaded");
+});
