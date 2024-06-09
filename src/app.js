05a4e759829a4130566ae41c01a69f43f19d5c9a
matlvy <145528395+matlvy@users.noreply.github.com>
2024-06-09 20:08:50 -0300

    1

--- src/app.js
+++ src/app.js
@@ -1,38 +1,15 @@
 import express from "express";
+import productsRouter from "./routes/products.js";
+import cartsRouter from "./routes/carts.js";
+
 const app = express();
 const PORT = 8080;
 
-import petsRouter from "./routes/pets.router.js";
-import usersRouter from "./routes/users.router.js";
-
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+
+app.use("/api/products", productsRouter);
+app.use("/api/carts", cartsRouter);
 
 app.listen(PORT, () => {
-  console.log("listening at the port:", PORT);
-});
-
-app.get("/", (req, res) => {
-  res.send("welcome to my app");
-});
-app.use("/api/pets", petsRouter);
-app.use("/api/users", usersRouter);
-
-app.use(express.static("./src/public"));
-
-import multer from "multer";
-
-const storage = multer.diskStorage({
-  destination: (req2, file2, cb2) => {
-    cb(null, "src/public/img");
-  },
-  filename: (req3, file3, cb3) => {
-    cb(null, file.originalname);
-  },
-});
-
-const upload = multer({ storage });
-
-app.post("/upload", upload.single("image"), (req, res) => {
-  res.send("uploaded");
+  console.log(`Server is listening on port ${PORT}`);
 });
