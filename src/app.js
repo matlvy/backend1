ce84411bf02612f455cfc264a11cfed58488e29f
matlvy <145528395+matlvy@users.noreply.github.com>
2024-06-18 23:34:03 -0300

    11

--- src/app.js
+++ src/app.js
@@ -4,3 +4,18 @@ import { Server } from "socket.io";
 
 const app = express();
 const PORT = 8080;
+
+app.use(express.static("./src/public"));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.engine("handlebards", exphbs.engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
+
+app.get("/", (req, res) => {
+  res.send("hi world");
+});
+app.listen(PORT, () => {
+  console.log("listening on port", PORT);
+});
