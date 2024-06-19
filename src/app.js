fff2ece1067c8ff7ba109afd06fb0c841f0b0a76
matlvy <145528395+matlvy@users.noreply.github.com>
2024-06-18 23:19:28 -0300

    11

--- src/app.js
+++ src/app.js
@@ -4,28 +4,3 @@ import { Server } from "socket.io";
 
 const app = express();
 const PORT = 8080;
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.static("./src/public"));
-
-app.engine("handlebars", exphbs.engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/views");
-
-app.use("/", viewsRouter);
-
-const httpServer = app.listen(PORT, () => {
-  console.log(`Server is listening on port ${PORT}`);
-});
-
-const io = new Server(httpServer);
-
-io.on("connection", (socket) => {
-  console.log("a client has connected");
-  socket.on("message", (data) => {
-    console.log(data);
-  });
-  socket.emit("greetings", "hi client, how are you? i'm your server");
-  socket.emit("users", users);
-});
