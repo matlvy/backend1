c99e5079b6f249c7aeccb4c0f0f140c8bf360cbd
matlvy <145528395+matlvy@users.noreply.github.com>
2024-06-20 00:12:56 -0300

    2

--- src/app.js
+++ src/app.js
@@ -1,6 +1,9 @@
 import express from "express";
+import { engine } from "express-handlebars";
+import { Server } from "socket.io";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
+import viewsRouter from "./routes/views.router.js";
 
 const app = express();
 const PUERTO = 8080;
@@ -8,11 +11,17 @@ const PUERTO = 8080;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.static(".src/public"));
+
+app.engine("handlebars", engine());
+app.set("view engine", "handlebars");
+app.set("views", "./src/views");
 
 // Rutas
 app.use("/api/products", productsRouter);
 app.use("/api/carts", cartsRouter);
+app.use("/", viewsRouter);
 
 app.listen(PUERTO, () => {
-    console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
---
+++ src/routes/views.router.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+const router = Router();
+
+router.get("/realtimeproducts", async (req, res) => {
+  res.render("realtimeproducts");
+});
+
+export default router;
