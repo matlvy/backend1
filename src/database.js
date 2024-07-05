0cb3d9d866879d7842ff60b1c122173faabc0678
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-04 21:22:11 -0300

    16

--- src/database.js
+++
@@ -1,7 +0,0 @@
-import mongoose from "mongoose";
-mongoose
-  .connect(
-    "mongodb+srv://mattlevyprg:coderhouse@cluster0.0nikfi0.mongodb.net/coderest"
-  )
-  .then(() => console.log("conectado a mongodb"))
-  .catch((error) => console.log("tenemos un error", error));
