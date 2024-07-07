db4816e5be3d46c8f6ce92a69d02a182fdb88dc7
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-07 03:39:01 -0300

    A3

--- src/app.js
+++ src/app.js
@@ -4,6 +4,7 @@ import { Server } from "socket.io";
 import productsRouter from "./routes/products.router.js";
 import cartsRouter from "./routes/carts.router.js";
 import viewsRouter from "./routes/views.router.js";
+import "./database.js";
 
 const app = express();
 const PUERTO = 8080;
@@ -11,12 +12,12 @@ const PUERTO = 8080;
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-app.use(express.static("./src/public"));
+app.use(express.static("./src/public")); 
 
 //Express-Handlebars
-app.engine("handlebars", engine());
-app.set("view engine", "handlebars");
-app.set("views", "./src/views");
+app.engine("handlebars", engine()); 
+app.set("view engine", "handlebars"); 
+app.set("views", "./src/views"); 
 
 // Rutas
 app.use("/api/products", productsRouter);
@@ -24,28 +25,33 @@ app.use("/api/carts", cartsRouter);
 app.use("/", viewsRouter);
 
 const httpServer = app.listen(PUERTO, () => {
-  console.log(`Servidor escuchando en el puerto ${PUERTO}`);
+    console.log(`Servidor escuchando en el puerto ${PUERTO}`);
 });
 
-import ProductManager from "./controllers/product-manager.js";
+import ProductManager from "./dao/fs/product-manager.js";
 const productManager = new ProductManager("./src/models/productos.json");
 
-const io = new Server(httpServer);
+const io = new Server(httpServer); 
+
 
 io.on("connection", async (socket) => {
-  console.log("Un cliente se conecto");
+    console.log("Un cliente se conecto"); 
 
-  socket.emit("productos", await productManager.getProducts());
+    //Enviamos el array de productos: 
+    socket.emit("productos", await productManager.getProducts());
 
-  socket.on("eliminarProducto", async (id) => {
-    await productManager.deleteProduct(id);
+    //Recibimos el evento "eliminarProducto" desde el cliente: 
+    socket.on("eliminarProducto", async (id) => {
+        await productManager.deleteProduct(id);
 
-    io.sockets.emit("productos", await productManager.getProducts());
-  });
+        //Le voy a enviar la lista actualizada al cliente: 
+        io.sockets.emit("productos", await productManager.getProducts());
+    })
 
-  socket.on("agregarProducto", async (producto) => {
-    await productManager.addProduct(producto);
-
-    io.sockets.emit("productos", await productManager.getProducts());
-  });
-});
+    //Agregamos productos por medio de un formulario: 
+    socket.on("agregarProducto", async (producto) => {
+        await productManager.addProduct(producto); 
+        //Le voy a enviar la lista actualizada al cliente: 
+        io.sockets.emit("productos", await productManager.getProducts());
+    })
+})
