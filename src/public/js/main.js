f55402d49a790017fef116426166e30e8eaf3386
matlvy <145528395+matlvy@users.noreply.github.com>
2024-07-11 02:04:36 -0300

    17

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,53 +1,53 @@
-const socket = io(); 
+const socket = io();
 
 socket.on("productos", (data) => {
-    renderProductos(data);
-})
+  renderProductos(data);
+});
 
-//Función para renderizar nuestros productos: 
+//Función para renderizar nuestros productos:
 
 const renderProductos = (data) => {
-    const contenedorProductos = document.getElementById("contenedorProductos");
-    contenedorProductos.innerHTML = "";
+  const contenedorProductos = document.getElementById("contenedorProductos");
+  contenedorProductos.innerHTML = "";
 
-    data.forEach(item => {
-        const card = document.createElement("div"); 
-        
-        card.innerHTML = `  <p> ${item.id} </p>
-                            <p> ${item.title} </p>
+  data.forEach((item) => {
+    const card = document.createElement("div");
+
+    card.innerHTML = `  <p> ${item.title} </p>
+                            <p> ${item.description} </p>
                             <p> ${item.price} </p>
+                            <p> ${item.category} </p>
                             <button> Eliminar </button>
-                        `
-        contenedorProductos.appendChild(card); 
-        //Agregamos un evento al boton de eliminar: 
-        card.querySelector("button").addEventListener("click", () => {
-            eliminarProducto(item.id); 
-        })
-    })
-}
+                        `;
+    contenedorProductos.appendChild(card);
+    //Agregamos un evento al boton de eliminar:
+    card.querySelector("button").addEventListener("click", () => {
+      eliminarProducto(item.id);
+    });
+  });
+};
 
 const eliminarProducto = (id) => {
-    socket.emit("eliminarProducto", id); 
-}
+  socket.emit("eliminarProducto", id);
+};
 
-//Agregamos productos con el formulario: 
+//Agregamos productos con el formulario:
 
 document.getElementById("btnEnviar").addEventListener("click", () => {
-    agregarProducto();
-})
-
+  agregarProducto();
+});
 
 const agregarProducto = () => {
-    const producto = {
-        title: document.getElementById("title").value,
-        description: document.getElementById("description").value,
-        price: document.getElementById("price").value,
-        img: document.getElementById("img").value,
-        code: document.getElementById("code").value,
-        stock: document.getElementById("stock").value,
-        category: document.getElementById("category").value,
-        status: document.getElementById("status").value === "true",
-    }
+  const producto = {
+    title: document.getElementById("title").value,
+    description: document.getElementById("description").value,
+    price: document.getElementById("price").value,
+    img: document.getElementById("img").value,
+    code: document.getElementById("code").value,
+    stock: document.getElementById("stock").value,
+    category: document.getElementById("category").value,
+    status: document.getElementById("status").value === "true",
+  };
 
-    socket.emit("agregarProducto", producto);
-}
+  socket.emit("agregarProducto", producto);
+};
